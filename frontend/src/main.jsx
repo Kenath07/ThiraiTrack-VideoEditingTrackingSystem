fb43ac7c5f5ec2c3205464a9cfeb501a8a81409c
J Kenath <kenathkenady@gmail.com>
2026-04-29 20:41:48 +0530

    Configure React entry point and global styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// React entry point and global styles
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
